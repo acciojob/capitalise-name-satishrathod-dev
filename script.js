30ff0e49418e0338228691b88995e8564089557d
satishrathod-dev <167071223+satishrathod-dev@users.noreply.github.com>
2024-05-20 16:48:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//your JS code here. If required.
-function upperCase(input) {
-	return input.toUpppercase();
-}
+// //your JS code here. If required.  
+// function upperCase(input) {
+// 	return input.toUpppercase();
+// }
 
+
+document.getElementById('fname').addEventListener('blur', function()) {
+	this.value = this.value.toUpperCase();
+}
